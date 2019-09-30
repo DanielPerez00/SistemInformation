cca4d8175e560c00c2dd78d872fd237b88ec797f
DanielPErez00 <al02850385@tecmilenio.mx>
2019-09-30 14:10:15 -0500

    Modificacion a la clase Network

--- NetworkInfo.java
+++ NetworkInfo.java
@@ -1,6 +1,27 @@
+import java.net.Socket;
+
 public class NetworkInfo{
     public static void getNetworkInfo(){
         System.out.println("\n\n*****Network Information*****\n");
+        NetworkInfo.getConexionStatus();
+        
+
+        }
+
+    public static void getConexionStatus(){
+
+        String Web = "www.tecmilenio.mx";
+        int puerto = 80;
+
+        try {
+                Socket s = new Socket(Web, puerto) ;
+                if (s.isConnected()){
+                    System.out.println("Conexion a " + Web + " a travez del puerto "+ puerto);
+                }}
+
+        catch (Exception e){
+            System.err.println("No se pudo establecer conexion");
+
     }
     
-}
+}}
